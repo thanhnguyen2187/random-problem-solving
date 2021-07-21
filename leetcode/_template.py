8ef37803c030376af60cb7d94d605f462edd4ecf
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-07-21 19:55:33 +0700

    Create Leetcode template

---
+++ leetcode/_template.py
@@ -0,0 +1,27 @@
+from typing import (
+    List,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    product,
+    accumulate,
+    takewhile,
+    islice,
+    chain,
+    cycle,
+    repeat,
+)
+
+
+class Solution:
+    def f(
+        self,
+    ) -> int:
+        ...
+
+
+if __name__ == '__main__':
+    solution = Solution()
