f22c58d7724de5d76b49e3613baceae9c4bdb3d7
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-07-28 16:35:23 +0700

    Add common imports to template

--- leetcode/_template.py
+++ leetcode/_template.py
@@ -1,19 +1,32 @@
 from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
     List,
+    NamedTuple,
     Optional,
+    Set,
+    Union,
 )
 from bisect import (
     bisect_left,
     bisect_right,
 )
 from itertools import (
-    product,
     accumulate,
-    takewhile,
-    islice,
     chain,
     cycle,
+    islice,
+    permutations,
+    product,
     repeat,
+    takewhile,
+)
+from functools import (
+    cached_property,
+)
+from collections import (
+    defaultdict,
 )
 
 
