8613c4b05c0b88cba87c24dad4d920826dc318e2
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-07-30 21:22:14 +0700

    Implement Leetcode 104

---
+++ leetcode/n0104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,78 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+)
+
+
+class ListNode:
+    def __init__(
+            self,
+            x: int,
+            next: Optional['ListNode'] = None,
+    ):
+        self.val: int = x
+        self.next: 'ListNode' = next
+
+
+class TreeNode:
+    def __init__(
+        self,
+        val: int = 0,
+        left: 'TreeNode' = None,
+        right: 'TreeNode' = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def maxDepth(
+        self,
+        root: TreeNode,
+    ) -> int:
+
+        if root is None:
+            return 0
+        if (
+            root.left is None and
+            root.right is None
+        ):
+            return 1
+
+        return max(
+            1 + self.maxDepth(root=root.left),
+            1 + self.maxDepth(root=root.right),
+        )
+
+
+if __name__ == '__main__':
+    solution = Solution()
