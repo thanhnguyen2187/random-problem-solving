a5ade42e96ed964282090aba79e316d5f3518f5d
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-08-11 20:44:26 +0700

    Implement Leetcode 429

---
+++ leetcode/n0429_n-ary_tree_level_order_traversal.py
@@ -0,0 +1,101 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+)
+
+
+class ListNode:
+    def __init__(
+            self,
+            x: int,
+            next: Optional['ListNode'] = None,
+    ):
+        self.val: int = x
+        self.next: 'ListNode' = next
+
+
+class TreeNode:
+    def __init__(
+        self,
+        val: int = 0,
+        left: 'TreeNode' = None,
+        right: 'TreeNode' = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Node:
+    def __init__(
+        self,
+        val: int = None,
+        children: List['Node'] = None,
+    ):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+
+    def traverse(
+        self,
+        # root: Node,
+        root,
+        level: int,
+        nodes_dict: DefaultDict[int, List[int]],
+    ):
+        if root is not None:
+            nodes_dict[level].append(root.val)
+            for child in root.children:
+                self.traverse(
+                    root=child,
+                    level=level + 1,
+                    nodes_dict=nodes_dict,
+                )
+
+    def levelOrder(
+        self,
+        # root: 'Node',
+        root,
+    ) -> List[List[int]]:
+        nodes_dict = defaultdict(list)
+        self.traverse(
+            root=root,
+            level=0,
+            nodes_dict=nodes_dict,
+        )
+        return list(
+            nodes_dict.values()
+        )
+
+
+if __name__ == '__main__':
+    solution = Solution()
