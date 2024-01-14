048d997f72d527ac3d7e792e83f67d5b428cf2e0
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-01-14 17:13:08 +0700

    Update Leetcode 22

--- leetcode/n0022_generate_parentheses.py
+++ leetcode/n0022_generate_parentheses.py
@@ -34,75 +34,46 @@ from collections import (
 )
 
 
-def generate(
-    left_count: int,
-    right_count: int,
-    current: str,
-    result: Set,
-):
-    if left_count == 0 and right_count == 0:
-        result.add(current)
-    elif left_count == 0:
-        generate(
-            left_count=0,
-            right_count=0,
-            current=current + (")" * right_count),
-            result=result,
-        )
-    elif left_count == right_count:
-        for new_current in {
-            current + "(",
-            "(" + current,
-        }:
-            generate(
-                left_count=left_count - 1,
-                right_count=right_count,
-                current=new_current,
-                result=result,
-            )
-    elif left_count > right_count:
-        for new_current in {
-            current + "(",
-            "(" + current,
-        }:
-            generate(
-                left_count=left_count - 1,
-                right_count=right_count,
-                current=new_current,
-                result=result,
-            )
-        generate(
-            left_count=left_count,
-            right_count=right_count - 1,
-            current=current + ")",
-            result=result,
-        )
-    elif left_count < right_count:
-        generate(
-            left_count=left_count - 1,
-            right_count=right_count,
-            current="(" + current,
-            result=result,
-        )
-
-
-#
-# (
-# () ((
-
-
-
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = set()
-        generate(left_count=n, right_count=n, result=result, current="")
+
+        def recurse(
+            left_count: int,
+            right_count: int,
+            current: str,
+        ):
+            if left_count == n and right_count == n:
+                result.add(current)
+            elif left_count == n:
+                new_current = current + (")" * (n - right_count))
+                result.add(new_current)
+            elif left_count == right_count:
+                recurse(
+                    left_count=left_count + 1,
+                    right_count=right_count,
+                    current=current + "(",
+                )
+            elif left_count > right_count:
+                recurse(
+                    left_count=left_count + 1,
+                    right_count=right_count,
+                    current=current + "(",
+                )
+                recurse(
+                    left_count=left_count,
+                    right_count=right_count + 1,
+                    current=current + ")",
+                )
+
+        recurse(left_count=0, right_count=0, current="")
 
         return result
 
 
 if __name__ == '__main__':
     solution = Solution()
-    # print(solution.generateParenthesis(n=1))
+    print(solution.generateParenthesis(n=1))
     print(solution.generateParenthesis(n=2))
-    # print(solution.generateParenthesis(n=3))
-    # print(solution.generateParenthesis(n=4))
+    print(solution.generateParenthesis(n=3))
+    print(solution.generateParenthesis(n=4))
