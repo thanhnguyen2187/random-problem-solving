4cc5b573c9cd737c97e8f89f3110a44e76dfe6cf
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-02-24 14:58:18 +0700

    Implement Leetcode 287

---
+++ leetcode/n0287_find_the_duplicated_number.py
@@ -0,0 +1,56 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    combinations,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+    Counter,
+)
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = 0
+        fast = 0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow_2 = 0
+        while slow != slow_2:
+            slow = nums[slow]
+            slow_2 = nums[slow_2]
+
+        return slow
+
+
+if __name__ == '__main__':
+    solution = Solution()
