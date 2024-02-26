c5aa7dae7f0719ae5269182ef56ac78e1d610e59
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-02-26 20:44:18 +0700

    Implement Leetcode 235

---
+++ leetcode/n0235_lowest_common_ancestor.py
@@ -0,0 +1,60 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    combinations,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cache,
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+    Counter,
+)
+
+
+class TreeNode:
+    def __init__(
+        self,
+        val: int = 0,
+        left: 'TreeNode' = None,
+        right: 'TreeNode' = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        def find_path(root: TreeNode, dest: TreeNode, result: List[TreeNode]):
+            if root is None or root is dest:
+                return
+
+            for aux in (root.left, root.right):
+                ...
+
+
+if __name__ == '__main__':
+    solution = Solution()
