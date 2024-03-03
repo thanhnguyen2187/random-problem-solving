1940f2f68be5cd90cc0db3f90c662da8d5d0a36f
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-03-03 23:28:37 +0700

    Implement Leetcode 212

--- leetcode/n0212_word_search_ii.py
+++ leetcode/n0212_word_search_ii.py
@@ -35,7 +35,10 @@ from collections import (
 
 
 class TrieNode:
-    def __init__(self, char):
+    char: str
+    next_nodes: dict
+
+    def __init__(self, char: str):
         self.char = char
         self.next_nodes = {}
 
@@ -44,10 +47,12 @@ class TrieNode:
 
 
 class Solution:
+    start_node: TrieNode
+    stop_node: TrieNode
 
     def __init__(self):
-        self.start_node = TrieNode(char='^')
-        self.stop_node = TrieNode(char='$')
+        self.start_node = TrieNode('^')
+        self.stop_node = TrieNode('$')
 
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         for word in words:
@@ -61,62 +66,56 @@ class Solution:
                 cursor.add_next(node=node)
                 cursor = node
 
-            node.add_next(node=self.stop_node)
+            cursor.add_next(node=self.stop_node)
 
+        result = set()
         m = len(board)
         n = len(board[0])
-        result = []
-        visited = set()
-        def recurse(i: int, j: int, node: TrieNode, word: str):
-            if not (0 <= i < m) or not (0 <= j < n):
-                return
+        def recurse(cursor: TrieNode, i: int, j: int, word: str):
             if (i, j) in visited:
                 return
-
-            visited.add((i, j))
-            char = board[i][j]
-            if char not in node.next_nodes:
-                visited.remove((i, j))
+            if not (0 <= i < m and 0 <= j < n):
                 return
 
-            next_node = node.next_nodes[char]
-            if '$' in node.next_nodes:
-                result.append(word + char)
-            next_positions = [
-                (i, j + 1),
-                (i, j - 1),
-                (i + 1, j),
-                (i - 1, j),
-            ]
-            for k, l in next_positions:
-                recurse(i=k, j=l, node=next_node, word=word + char)
-
+            char = board[i][j]
+            word = word + char
+            visited.add((i, j))
+            if char in cursor.next_nodes:
+                cursor = cursor.next_nodes[char]
+                if '$' in cursor.next_nodes:
+                    result.add(word)
+                for k, l in (
+                    (i + 1, j),
+                    (i - 1, j),
+                    (i, j + 1),
+                    (i, j - 1),
+                ):
+                    recurse(cursor=cursor, i=k, j=l, word=word)
             visited.remove((i, j))
 
         for i, j in product(range(m), range(n)):
-            visited.clear()
-            recurse(i=i, j=j, node=self.start_node, word="")
+            visited = set()
+            recurse(cursor=self.start_node, i=i, j=j, word="")
 
         return result
 
 
-
 if __name__ == '__main__':
     solution = Solution()
-    # print(solution.findWords(
-    #     board=[
-    #         ["o", "a", "a", "n"],
-    #         ["e", "t", "a", "e"],
-    #         ["i", "h", "k", "r"],
-    #         ["i", "f", "l", "v"],
-    #     ],
-    #     words=["oath", "pea", "eat", "rain", "oao", "oaan"],
-    # ))
+    print(solution.findWords(
+        board=[
+            ["o", "a", "a", "n"],
+            ["e", "t", "a", "e"],
+            ["i", "h", "k", "r"],
+            ["i", "f", "l", "v"],
+        ],
+        words=["oath", "pea", "eat", "rain", "oao", "oaan"],
+    ))
     # print(solution.findWords(
     #     board=[["a"]],
     #     words=["a"],
     # ))
-    print(solution.findWords(
-        board=[["a", "b", "c"], ["a", "e", "d"], ["a", "f", "g"]],
-        words=["abcdefg", "gfedcbaaa", "eaabcdgfa", "befa", "dgc", "ade"],
-    ))
+    # print(solution.findWords(
+    #     board=[["a", "b", "c"], ["a", "e", "d"], ["a", "f", "g"]],
+    #     words=["abcdefg", "gfedcbaaa", "eaabcdgfa", "befa", "dgc", "ade"],
+    # ))
