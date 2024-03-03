b18418a0a11119a80542d2c57662b2b4f2fc3ac5
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-03-03 23:28:49 +0700

    Implement Leetcode 215

---
+++ leetcode/n0215_kth_largest_element_in_an_array.py
@@ -0,0 +1,53 @@
+import heapq
+
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    combinations,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cache,
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+    Counter,
+)
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums = [-num for num in nums]
+        heapq.heapify(nums)
+
+        result = 0
+        while k > 0:
+            result = heapq.heappop(nums)
+            k -= 1
+
+        return -result
+
+
+if __name__ == '__main__':
+    solution = Solution()
