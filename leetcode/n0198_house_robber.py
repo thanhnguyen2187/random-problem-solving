c495b8f8ad9ffb9b3ae50732fc0540eabda30cd2
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-03-03 23:28:18 +0700

    Implement Leetcode 198

---
+++ leetcode/n0198_house_robber.py
@@ -0,0 +1,58 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    combinations,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cache,
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+    Counter,
+)
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        result = list(repeat(0, n))
+        result[0] = nums[0]
+        result[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            result[i] = max(
+                nums[i] + result[i - 2],
+                result[i - 1],
+            )
+
+        return result[-1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    # print(solution.rob(nums=[1, 2, 3, 1]))
+    print(solution.rob(nums=[2, 7, 9, 3, 1]))
