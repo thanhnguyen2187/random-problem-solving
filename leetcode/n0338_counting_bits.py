1b89ade0a2fd50ca62f0d0962c97eb5f31c6e379
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-03-06 23:55:56 +0700

    Implement Leetcode 338

---
+++ leetcode/n0338_counting_bits.py
@@ -0,0 +1,47 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    combinations,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cache,
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+    Counter,
+)
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        result = [
+            bin(i).count('1')
+            for i in range(0, n + 1)
+        ]
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
