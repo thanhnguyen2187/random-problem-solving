d5bd887be273f774cbc8c1e3ec0c3b1c65f6c736
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-03-06 23:55:47 +0700

    Implement Leetcode 191

---
+++ leetcode/n0191_number_of_1_bits.py
@@ -0,0 +1,43 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    combinations,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cache,
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+    Counter,
+)
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return bin(n).count("1")
+
+
+if __name__ == '__main__':
+    solution = Solution()
