27791f014d79514ad6076aaf8461912ee0856235
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-03-09 09:49:10 +0700

    Implement Leetcode 43

---
+++ leetcode/n0043_multiply_strings_2.py
@@ -0,0 +1,43 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    combinations,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cache,
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+    Counter,
+)
+
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1) * int(num2))
+
+
+if __name__ == '__main__':
+    solution = Solution()
