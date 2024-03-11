a36a955f1179804d2c5458978ce555b98fc371ba
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-03-11 18:37:23 +0700

    Implement Leetcode 349

---
+++ leetcode/n0349_intersection_of_two_arrays.py
@@ -0,0 +1,43 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    combinations,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cache,
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+    Counter,
+)
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return set(nums1).intersection(nums2)
+
+
+if __name__ == '__main__':
+    solution = Solution()
