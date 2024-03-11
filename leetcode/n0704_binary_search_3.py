0f5b5032ce14f4a3cf1abaece7f8169ad2756d43
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-03-11 18:37:30 +0700

    Implement Leetcode 704 (third time)

---
+++ leetcode/n0704_binary_search_3.py
@@ -0,0 +1,55 @@
+from typing import (
+    DefaultDict,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Set,
+    Union,
+)
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+from itertools import (
+    accumulate,
+    chain,
+    combinations,
+    cycle,
+    islice,
+    permutations,
+    product,
+    repeat,
+    takewhile,
+)
+from functools import (
+    cache,
+    cached_property,
+)
+from collections import (
+    defaultdict,
+    deque,
+    Counter,
+)
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        l, r = 0, n - 1
+        while l <= r:
+            m = (l + r) // 2
+            num = nums[m]
+            if num == target:
+                return m
+            elif num < target:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
