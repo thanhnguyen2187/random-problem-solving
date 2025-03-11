3272dab9335543eb82318a53e8efd6e97a3b07a7
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-03-11 17:31:04 +0700

    neetcode: add template file

---
+++ neetcode/take_1/n000_template.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def solution(self, nums: List[int]) -> bool:
+        return True
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = []
+    result = s.solution(nums=nums)
+    print(result)
